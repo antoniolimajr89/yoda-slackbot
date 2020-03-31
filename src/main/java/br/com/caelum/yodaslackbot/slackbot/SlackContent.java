efb6141b2a1fbddf6748fa0a5bda26edd06705c2
Gabriel Ronei <gabriheron@hotmail.com>
2020-03-31 18:25:11 -0300

    .

---
+++ src/main/java/br/com/caelum/yodaslackbot/slackbot/SlackContent.java
@@ -0,0 +1,41 @@
+package br.com.caelum.yodaslackbot.slackbot;
+
+public class WakeUp {
+
+    private String token;
+    private String text;
+    private String channel;
+    private String username;
+    private String icon_emoji;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getIcon_emoji() {
+        return icon_emoji;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getChannel() {
+        return channel;
+    }
+
+    @Override
+    public String toString() {
+        return "WakeUp{" +
+                "token='" + token + '\'' +
+                ", text='" + text + '\'' +
+                ", channel='" + channel + '\'' +
+                ", bot_name='" + username + '\'' +
+                ", bot_icon='" + icon_emoji + '\'' +
+                '}';
+    }
+}
